1ae1f3059f09c2ed46c7ad948261ee3499760140
Juha <juha@coodari.com>
2017-07-28 12:00:40 +0300

    Added kontena deployment stage

---
+++ test/deploy-kontena.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+
+# do nothing if the build is for tagging a prod release
+if [ -n "$TRAVIS_TAG" ]; then
+    echo "*** Tagged build:" $TRAVIS_TAG
+    exit 0;
+fi
+
+set -e
+ORG=${ORG:-codeverde}
+
+kontena version
