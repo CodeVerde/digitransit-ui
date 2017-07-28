0f0abb1eca450c83d1b7cc5fb3e436b4f65a0105
Juha <juha@coodari.com>
2017-07-28 13:26:35 +0300

    Moar Kontena stuff

--- test/deploy-kontena.sh
+++ test/deploy-kontena.sh
@@ -10,3 +10,7 @@ set -e
 ORG=${ORG:-codeverde}
 
 kontena version
+kontena master login --token $KONTENA_LOGIN_TOKEN --grid $KONTENA_GRID https://$KONTENA_MASTER_IP
+kontena vault update -u FRONT_SERVICE_VERSION ci-$TRAVIS_BUILD_NUMBER
+if kontena stack list | grep -q article-project ; then kontena stack upgrade article-project ./descriptors/kontena-test-areas.yml ; fi
+if ! kontena stack list | grep -q article-project ; then kontena stack install ./descriptors/kontena-test-areas.yml ; fi
