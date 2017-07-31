95fb022591ad32019ad3841c4573c33e110985fe
Juha <juha@coodari.com>
2017-07-31 08:27:06 +0300

    Kontena deployment script added

--- test/deploy-kontena.sh
+++ test/deploy-kontena.sh
@@ -1,16 +1,9 @@
 #/bin/bash
-
-# do nothing if the build is for tagging a prod release
-if [ -n "$TRAVIS_TAG" ]; then
-    echo "*** Tagged build:" $TRAVIS_TAG
-    exit 0;
-fi
-
 set -e
-ORG=${ORG:-codeverde}
+ORG=${ORG:-hsldevcom}
 
-kontena version
 kontena master login --token $KONTENA_LOGIN_TOKEN --grid $KONTENA_GRID https://$KONTENA_MASTER_IP
-kontena vault update -u FRONT_SERVICE_VERSION ci-$TRAVIS_BUILD_NUMBER
-if kontena stack list | grep -q article-project ; then kontena stack upgrade article-project ./descriptors/kontena-test-areas.yml ; fi
-if ! kontena stack list | grep -q article-project ; then kontena stack install ./descriptors/kontena-test-areas.yml ; fi
+
+kontena vault update -u FRONT_SERVICE_VERSION $KONTENA_FRONT_SERVICE_VERSION
+if kontena stack list | grep -q $KONTENA_STACK_NAME ; then kontena stack upgrade $KONTENA_STACK_NAME ./kontena.yml ; fi
+if ! kontena stack list | grep -q $KONTENA_STACK_NAME ; then kontena stack install ./kontena.yml ; fi
