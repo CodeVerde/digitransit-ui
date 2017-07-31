aa1ddadfaf32912a35be4afc7740f4444c74ee94
Juha <juha@coodari.com>
2017-07-31 09:51:23 +0300

    Kontena deployment script added, changed Docker container tagging scheme a bit

--- test/deploy-kontena.sh
+++ test/deploy-kontena.sh
@@ -1,9 +1,13 @@
 #/bin/bash
+
+# Skip the deployment for PRs
+- if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then exit 0; fi
+
 set -e
 ORG=${ORG:-hsldevcom}
 
 kontena master login --token $KONTENA_LOGIN_TOKEN --grid $KONTENA_GRID https://$KONTENA_MASTER_IP
 
-kontena vault update -u FRONT_SERVICE_VERSION $KONTENA_FRONT_SERVICE_VERSION
+kontena vault update -u FRONT_SERVICE_VERSION ci-$TRAVIS_BUILD_NUMBER
 if kontena stack list | grep -q $KONTENA_STACK_NAME ; then kontena stack upgrade $KONTENA_STACK_NAME ./kontena.yml ; fi
 if ! kontena stack list | grep -q $KONTENA_STACK_NAME ; then kontena stack install ./kontena.yml ; fi
